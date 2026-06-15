14c3d06a893c84cb6592193bbad16a01bd7a7348
zbmzubayer <zbmzubayer@gmail.com>
2026-06-16 02:44:07 +0600

    fixed: (issues - 12) <section> layout wrapper + unnecessary cn() in AboutSection

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,17 @@
-import Navbar from "@/components/common/Navbar";
-import AboutSection from "@/components/about-us/AboutSection";
+import IntroSection from "@/components/about-us/IntroSection";
+import OurStory from "@/components/about-us/OurStory";
+import OurValueSection from "@/components/about-us/OurValueSection";
+import OurTeamSection from "@/components/about-us/OurTeamSection";
+import LifeAtSection from "@/components/about-us/LifeAtSection";
 
 export default function AboutPage() {
   return (
-    <div className="lofi-page">
-      <Navbar />
-      <AboutSection />
+    <div className="lofi-page bg-[#faf7f2] px-[calc(100%/12)] pb-[160px] py-32">
+      <IntroSection />
+      <OurStory />
+      <OurValueSection />
+      <OurTeamSection />
+      <LifeAtSection />
     </div>
   );
 }
--- src/data/about.ts
+++
@@ -1,1 +0,0 @@
-export * from "@/data/about/index";
